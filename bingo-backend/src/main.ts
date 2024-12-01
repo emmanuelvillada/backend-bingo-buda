2f45995f3f4a502a594a435b4cba320a0839038f
Emmanuel Villada Suarez <emmanuelvillada1903@gmail.com>
2024-12-01 03:51:30 -0500

    structuring the project

--- bingo-backend/src/main.ts
+++ bingo-backend/src/main.ts
@@ -1,6 +1,10 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
